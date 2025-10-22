91d8cebfe5e0435a220582b1e14e62d70622519a
Alejandro Bueno <alejandrobuenocur@gmail.com>
2025-10-22 12:32:19 +0200

    Cambio en layers
    Para que el personaje pueda pasar por debajo de los sprites q estan en la layer2

--- src/level1.js
+++ src/level1.js
@@ -11,29 +11,58 @@ export default class Level1 extends Phaser.Scene{
 
     create(){
         this.createAnims();
-        //Informacion del mapa
-        const mapData = [
-            [0, 1, 2, 3, 4, 5],
-            [10, 11, 12, 13, 14, 15],
-            [20, 21, 22, 23, 24, 25],
-            [30, 31, 32, 33, 34, 35],
-            [40, 41, 42, 43, 44, 45],
-        ];
+        //Index de los sprites del mapa
+        const TI = {
+            EIAR: 0, AR1: 1, AR2: 2, EDAR: 5,
+            PI1: 10, PI2: 20, PD1: 15, PD2: 25,
+            EIAB: 40, AB1: 41, AB2: 42, AB3: 51, AB4: 52, EDAB: 45,
+            S1: 11, S2: 12, S3: 13, S4: 14,
+            S5: 21, S6: 22, S7: 23, S8: 24,
+            S9: 31, S10: 32, S11: 33, S12: 34,
+            FF: 78,
+        };
 
         //Crea el mapa
         const map = this.make.tilemap({
-            //Coje la informacion del mapa
-            data: mapData,
-            //Coje las medidas de los sprites
+            //Se declara las medidas de los sprites
             tileWidth: 16,
-            tileHeight: 16});
-        
-        //Le dice al mapa que TileSet se usa para el mapa
+            tileHeight: 16,
+            width: 7,
+            height: 5
+            }
+        );
+        //Le declara el Tileset que se utilizara
         const tileset = map.addTilesetImage("TileSet");
-        //Crea la capa en la que esta el mapa
-        const layer = map.createLayer(0, tileset, 0, 0);
-        //Escalar los tyles de la escena
-        layer.setScale(5);
+
+        //Creamos el layer vacio
+        const layer1 = map.createBlankLayer("layer1", tileset, 0, 0);
+
+        //Crea el primer layer
+        const layer1Data = [
+            [TI.EIAR, TI.AR1, TI.AR2, TI.AR2, TI.AR1, TI.EDAR, TI.FF],
+            [TI.PI1, TI.S1, TI.S2, TI.S3, TI.S4, TI.PD1, TI.FF],
+            [TI.PI2, TI.S5, TI.S6, TI.S7, TI.S8, TI.PD2, TI.FF],
+            [TI.PI1, TI.S9, TI.S10, TI.S11, TI.S12, TI.PD2, TI.FF],
+            [-1, -1, -1, -1, -1, -1, TI.FF]
+        ];
+        layer1.putTilesAt(layer1Data, 0, 0);
+        layer1.setScale(5);
+        layer1.setDepth(0);
+        
+        // Crea el segundo layer vacio
+        const layer2 = map.createBlankLayer("layer2", tileset, 0, 0);
+
+        const layer2Data = [
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [TI.EIAB, TI.AB1, TI.AB2, TI.AB3, TI.AB4, TI.EDAB, -1]
+        ];
+        layer2.putTilesAt(layer2Data, 0, 0);
+        layer2.setScale(5);
+        layer2.setDepth(10);
+
 
 		this.add.text(240, 100, "Percival");
 		this.add.text(780, 100, "Daphne");
