09aed8e1e070e2c9d9ec30a4bbe25763244ca09e
InesUcm <inesdelp@ucm.es>
2025-11-06 23:33:26 +0100

    efecto visual

--- src/breakableObjects.js
+++ src/breakableObjects.js
@@ -2,13 +2,13 @@ import Players from "./players.js";
 
 export default class BreakableObject extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y,x2, y2, texture, percival, daphne){
+
         super(scene, x, y, texture)
         this.scene = scene
         this.percival = percival
         this.daphne = daphne
         this.textureKey = texture
         this.scale=0.12;
-
 
         //Caja1 (dimensión Percival) 
         this.scene.add.existing(this);
@@ -21,17 +21,13 @@ export default class BreakableObject extends Phaser.Physics.Arcade.Sprite{
         this.caja2.setScale(this.scale);
         this.caja2.body.immovable = true;
 
-        //ajustar el tamaño
-        //this.body.setSize(this.displayWidth, this.displayHeight);
-        //this.body.setOffset(-this.displayWidth/2, -this.displayHeight/2);
-
         //Colliders
         scene.physics.add.collider(this, percival);
         scene.physics.add.collider(this.caja2, daphne);
 
         // Estado inicial
         this.isBroken = false;
-        this.breakRange = 80; // distancia para poder romper
+        this.breakRange = 90; // distancia para poder romper
 
         // Asignar tecla SHIFT (para Percival)
         this.keyShift = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
@@ -48,6 +44,7 @@ export default class BreakableObject extends Phaser.Physics.Arcade.Sprite{
         const percivalCenterX = this.percival.body.x + this.percival.body.width / 2;
         const percivalCenterY = this.percival.body.y + this.percival.body.height / 2;
 
+        //Calcula la distancia entre la caja y percival
         const distance = Phaser.Math.Distance.Between(
             boxCenterX,
             boxCenterY,
@@ -55,24 +52,38 @@ export default class BreakableObject extends Phaser.Physics.Arcade.Sprite{
             percivalCenterY
         );
 
+        //Si percival se encuentra dentro del rango de distancia deja romper la caja
         if (distance < this.breakRange) {
-            // Mostrar que se puede romper
-            this.setTint(0xff8888);
+
+            // Muestra que se puede romper
+            if (distance < this.breakRange && !this.glowTween) {
+             this.glowTween = this.scene.tweens.add({
+             targets: this,
+             alpha: { from: 1, to: 0.2 },
+             duration: 800,
+             yoyo: true,
+             repeat: -1
+             });
+            } 
 
             // Si pulsa SHIFT → romper
             if (Phaser.Input.Keyboard.JustDown(this.keyShift)) {
                 this.breakObject();
             }
-        } else {
-            this.clearTint();
+        } 
+        else if (distance >= this.breakRange && this.glowTween) {
+            this.glowTween.stop();
+            this.glowTween = null;
+            this.alpha = 1;
         }
     }
+
     breakObject() {
         if (this.isBroken) return;
 
         this.isBroken = true;
 
-        // Cambiar textura o hacer animación
+        // Cambia la textura
         this.setTexture("cajaRota");
         this.caja2.setTexture("cajaRota");
 
