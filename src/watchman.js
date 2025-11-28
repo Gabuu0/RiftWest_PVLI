8c64595382851fb03c94f2161a1edf42d785b941
InesUcm <inesdelp@ucm.es>
2025-11-28 18:11:59 +0100

    error

--- src/watchman.js
+++ src/watchman.js
@@ -1,54 +1,39 @@
-export default class Watchman extends Phaser.Physics.Arcade.Sprite{
+export default class Watchman extends Phaser.GameObjects.PathFollower {
     constructor(scene, x, y, texture, player, pathPoints) {
-        super(scene, x, y, texture);
+
+        //Crea el path
+        const path = new Phaser.Curves.Path(pathPoints[0].x, pathPoints[0].y);
+
+        //Dibuja el path
+        for (let i = 1; i < pathPoints.length; i++) {
+            path.lineTo(pathPoints[i].x, pathPoints[i].y);
+        }
+
+        path.lineTo(pathPoints[0].x, pathPoints[0].y); //Une el ultimo punto con el primero
+
+        //Crea un PathFollower
+        super(scene, path, x, y, texture);
 
         this.scene = scene;
         this.player = player;
-        this.path = pathPoints;      // Array de posiciones
-        this.pathIndex = 0;          // Punto actual al que va
-        this.speed = 70;             // Velocidad del vigilante
-        this.visionDistance = 120;   // Cuánto ve
-        this.visionAngle = 45;       // Ángulo hacia delante
-        this.isAlert = false;
 
         scene.add.existing(this);
+        
         scene.physics.add.existing(this);
-
-        this.body.setScale(0.3);
         this.body.setImmovable(true);
+
+        this.setScale(0.4);
+
+        //Movimiento
+        this.startFollow({
+            duration: 10000,
+            yoyo: false,
+            repeat: -1,
+            rotateToPath: false
+        });
     }
 
-     preUpdate(t, dt) {
+    preUpdate(t, dt) {
         super.preUpdate(t, dt);
-
-        this.followPath();
-    }
-    
-    followPath() {
-        const target = this.path[this.pathIndex];
-
-        // Distancia hacia el punto objetivo
-        const dx = target.x - this.x;
-        const dy = target.y - this.y;
-        const dist = Math.hypot(dx, dy);
-
-        if (dist < 5) {
-            // Avanza al siguiente punto
-            this.pathIndex++;
-
-            // Si llegó al final → vuelve al principio
-            if (this.pathIndex >= this.path.length) {
-                this.pathIndex = 0;
-            }
-            return;
-        }
-
-        // Movimiento normalizado
-        const angle = Math.atan2(dy, dx);
-
-        this.body.setVelocity(
-            Math.cos(angle) * this.speed,
-            Math.sin(angle) * this.speed
-        );
     }
 }
