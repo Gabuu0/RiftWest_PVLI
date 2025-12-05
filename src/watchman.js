794b2b2c0c69493061de9103debb444d487d0f7d
InesUcm <inesdelp@ucm.es>
2025-12-05 11:54:12 +0100

    vigilante terminado

--- src/watchman.js
+++ src/watchman.js
@@ -25,7 +25,7 @@ export default class Watchman extends Phaser.GameObjects.PathFollower {
 
         this.setScale(0.4);
 
-        this.trigger= scene.add.circle(this.x, this.y, 120, 0x00ff00, 0.1);
+        this.trigger= scene.add.circle(this.x, this.y, 120, 0xFF0000, 0.1);
         scene.physics.add.existing(this.trigger);
 
         this.trigger.body.setCircle(120);
@@ -36,6 +36,11 @@ export default class Watchman extends Phaser.GameObjects.PathFollower {
         
         // Collider con el jugador
         this.scene.physics.add.overlap(this.trigger, this.player,() => this.onPlayerEnter(), null, this);
+
+        //Valores para calcular la direccion del vigilante
+        this.anteriorX = this.x;
+        this.anteriorY = this.y;
+        this.direccion; //Derecha=1, Abajo=2, Izquierda=3, Arriba=4
 
         //Movimiento
         this.startFollow({
@@ -48,11 +53,38 @@ export default class Watchman extends Phaser.GameObjects.PathFollower {
 
     preUpdate(t, dt) {
         super.preUpdate(t, dt);
+
+        //vemos la dirección
+        this.updateDireccion();
+
+        //actualizamos la nueva dirección
+        this.anteriorX=this.x;
+        this.anteriorY=this.y;
+
+        //Ajusta el area de visión a la posicion del vigilante
         this.trigger.x=this.x;
         this.trigger.y=this.y;
     }
 
     onPlayerEnter(){
-        console.log("jugador detectado");
+        if(this.player.x < this.x && this.direccion==1){console.log("REINICIAR PARTIDA");}
+        else if(this.player.x > this.x && this.direccion==3){console.log("REINICIAR PARTIDA");}
+        else if(this.player.y < this.y && this.direccion==2){console.log("REINICIAR PARTIDA");}
+        else if(this.player.y > this.y && this.direccion==4){console.log("REINICIAR PARTIDA");}
+    }
+
+    updateDireccion(){
+        const realX=this.anteriorX-this.x;
+        const realY=this.anteriorY-this.y;
+
+        //Comprueba si se mueve en horizontal o vertical
+        if (Math.abs(realX) > Math.abs(realY)) {
+            if (realX>0) this.direccion=1; //Va a la derecha
+            if (realX<0) this.direccion=3; //Va la izquierda
+        }
+        else{
+            if(realY>0) this.direccion=2; //Va hacia abajo
+            if(realY<0) this.direccion=4; //Va hacia arriba
+        }
     }
 }
