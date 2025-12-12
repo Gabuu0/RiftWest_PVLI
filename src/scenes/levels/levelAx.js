86d858078407b6faa719db89df84a7990155ca11
InesUcm <inesdelp@ucm.es>
2025-12-12 02:38:34 +0100

    levelAx casi arreglado

--- src/scenes/levels/levelAx.js
+++ src/scenes/levels/levelAx.js
@@ -15,8 +15,12 @@ export default class LevelAx extends Phaser.Scene {
         super({key: "levelAx"});
     }
 
-    init(){
-
+    init(data){
+        this.clownGetItemJokes = data.clownGetItemJokes;
+        this.clownGiveItemJokes = data.clownGiveItemJokes;
+        this.clownNoObjectGiven = data.clownNoObjectGiven;
+        this.clownNoObjectTaken = data.clownNoObjectTaken;
+        this.clownLast = data.clownLast;
     }
 
     create(){
@@ -31,10 +35,12 @@ export default class LevelAx extends Phaser.Scene {
         this.percival.setDepth(1);
         this.daphne.setDepth(1);
 
-        this.scene.launch('InventarioPercival',this.percival);
-        this.scene.launch('InventarioDaphne',this.daphne);
-        this.inventario1 = this.scene.get('InventarioPercival');
-        this.inventario2 = this.scene.get('InventarioDaphne');
+        this.scene.launch('InventarioPercival',{
+            player:this.percival,
+            playerScene: this});
+        this.scene.launch('InventarioDaphne',{
+            player:this.daphne,
+            playerScene: this});
 
         this.movementController = new Movement(this, this.percival, this.daphne);
 
@@ -44,6 +50,11 @@ export default class LevelAx extends Phaser.Scene {
         this.daphneCam = this.cameras.add(540,0,540,540,'DaphneCam');
         this.daphneCam.setViewport(540,0,540,540);
         this.daphneCam.startFollow(this.daphne);
+
+        this.random = new Phaser.Math.RandomDataGenerator();
+
+        this.dialog = new DialogText(this, {camera: this.percivalCam});
+        this.dialog.setDepth(10);
         
         //#endregion
 
@@ -204,6 +215,15 @@ export default class LevelAx extends Phaser.Scene {
         this.resetSound = this.sound.add('reset',{loop: false, volume: 0.3});
         this.music = this.sound.add('musicaLevelAx', {loop: true, volume:0.5});
         //#endregion
+
+        this.cooldowns = {
+            percival: 0,
+            daphne: 0
+        };
+        this.skillCooldownTime = 3000;
+
+        this.createUI(); 
+        this.getScenes();
     }
 
     update(t, dt){
@@ -211,6 +231,163 @@ export default class LevelAx extends Phaser.Scene {
             this.movementController.update();
         }
         this.updateSounds();
+    }
+
+    UseAbility(playerName, object, action) {
+        const now = this.time.now;
+
+        const abilityIcon = playerName === "percival" ? this.percivalAbility : this.daphneAbility;
+        const readyTexture = playerName === "percival" ? "percivalAbilityReady" : "daphneAbilityReady";
+        const usedTexture = playerName === "percival" ? "percivalAbilityUsed" : "daphneAbilityUsed";
+        const msg = playerName === "percival" ? this.msgPercival : this.msgDaphne;
+
+        if (now < this.cooldowns[playerName]) {
+            msg.setText("Habilidad recargando...");
+            console.log();
+            this.time.delayedCall(this.skillCooldownTime, () => msg.setText(""));
+            return;
+        }
+
+        if (playerName === "percival") {
+            if (action.inRange) {object.breakObject(); 
+            this.cooldowns[playerName] = now + this.skillCooldownTime;
+            abilityIcon.setTexture(usedTexture);
+            this.time.delayedCall(this.skillCooldownTime, () => abilityIcon.setTexture(readyTexture));
+            }
+            else {
+                msg.setText("No hay nada que romper.");
+                this.time.delayedCall(1000, () => msg.setText(""));
+            } 
+        } 
+        else if (playerName === "daphne") {
+            if (action.isDropping) {
+                object.restartCoolDown(); // esto hace toggle, deja el objeto
+                this.cooldowns[playerName] = now + this.skillCooldownTime;
+                abilityIcon.setTexture(usedTexture);
+                this.time.delayedCall(this.skillCooldownTime, () => abilityIcon.setTexture(readyTexture));
+            }
+            else if(action.inRange){
+                object.restartCoolDown();
+            }
+            else {
+                // por si acaso: mensaje corto (opcional)
+                msg.setText("No estás agarrando nada.");
+                this.time.delayedCall(1000, () => msg.setText(""));
+            }
+        }
+    }
+    createUI() {
+        // Mensajes flotantes
+        this.msgPercival = this.add.text(100, 500, "", {
+            fontSize: "16px",
+            color: "hsla(280, 3%, 82%, 1.00)",
+            fontStyle: "bold"
+        }).setScrollFactor(0).setDepth(2);
+
+        this.msgDaphne = this.add.text(220, 500, "", {
+            fontSize: "16px",
+            color: "hsla(280, 3%, 82%, 1.00)",
+            fontStyle: "bold"
+        }).setScrollFactor(0).setDepth(2);
+
+        // ABILITIES
+        this.percivalAbility = this.add.image(60, 480, "percivalAbilityReady");
+        this.percivalAbility.setScale(0.4);
+        this.percivalAbility.setDepth(4);
+        this.percivalAbility.setScrollFactor(0);
+
+        this.daphneAbility = this.add.image(480, 480, "daphneAbilityReady");
+        this.daphneAbility.setScale(0.4);
+        this.daphneAbility.setDepth(4);
+        this.daphneAbility.setScrollFactor(0);
+        
+        // HEADERS (Añadir los Headers de la UI para evitar errores de referencias nulas)
+        this.percivalHeader = this.add.image(60, 50, "percivalHead").setScale(0.4).setDepth(10).setScrollFactor(0);
+        this.daphneHeader = this.add.image(480, 50, "daphneHead").setScale(0.4).setDepth(10).setScrollFactor(0);
+        
+        // MAP ICONS (Añadir los Map Icons de la UI para evitar errores de referencias nulas)
+        this.percivalMapIcon = this.add.image(85, 50, "percivalMapEnabled").setScrollFactor(0).setDepth(10).setScale(0.4);
+        this.daphneMapIcon = this.add.image(455, 50, "daphneMapEnabled").setScrollFactor(0).setDepth(10).setScale(0.4);
+        this.daphneMap = this.add.image(580, 150, "daphneMap").setVisible(false);
+        this.percivalMap = this.add.image(250, 150, "percivalMap").setVisible(false);
+        
+    }
+
+    setCamerasIgnores() {
+        this.percivalCam.ignore(this.msgDaphne);
+        this.percivalCam.ignore(this.daphneHeader);
+        this.percivalCam.ignore(this.daphneAbility);
+        this.percivalCam.ignore(this.daphneMapIcon);
+        this.percivalCam.ignore(this.daphneMap);
+
+        this.daphneCam.ignore(this.msgPercival);
+        this.daphneCam.ignore(this.percivalHeader);
+        this.daphneCam.ignore(this.percivalAbility);
+        this.daphneCam.ignore(this.percivalMapIcon);
+        this.daphneCam.ignore(this.percivalMap);
+    }
+
+    getScenes() {
+        this.inventario1 = this.scene.get('InventarioPercival');
+        this.inventario2 = this.scene.get('InventarioDaphne');
+        this.pauseMenu = this.scene.get('pauseMenu');
+    }
+
+    /**
+     * Este metodo se encarga de crear el dialogo del payaso al interactuar con él según los siguientes parámetros:
+     * @param {string} player - Personaje que ha interactuado con el payaso 
+     * @param {boolean} itemReceived - Si el personaje ha recibido un objeto del payaso 
+     * @param {boolean} hadItem - Si el personaje tiene un item seleccionado (en caso de dar un item al payaso)
+     * @param {boolean} clownEmpty - Si el payaso no tiene ningun objeto
+     * 
+     * Se usan los arrays de dialogos creados en {@link createClownDialogs()}
+     */
+    showClownMessage(player,itemReceived,hadItem = true,clownEmpty = false){
+        if(this.dialog.visible){
+            this.dialog.toggleWindow();
+        }
+
+        let ignorePlayer;
+        if(player === "percival"){
+            ignorePlayer = 1;
+        }else{
+            ignorePlayer = 2;
+        }
+         
+        this.dialog.toggleWindow();
+
+        this.setClownMessage(ignorePlayer,itemReceived,hadItem,clownEmpty);
+    }
+    /**
+     * Establece el texto del dialogo del payaso al interactuar con él
+     * @param {boolean} itemReceived - Si el personaje ha recibido un objeto del payaso 
+     * @param {boolean} hadItem - Si el personaje tiene un item seleccionado (en caso de dar un item al payaso)
+     * @param {boolean} clownEmpty - Si el payaso no tiene ningun objeto
+     */
+    setClownMessage(playerToIgnore,itemReceived,hadItem,clownEmpty){
+        if(itemReceived && !hadItem && !clownEmpty){
+            this.dialog.setTextArray([
+                [playerToIgnore,this.clownGetItemJokes[this.random.integerInRange(0, this.clownGetItemJokes.length-1)]],
+                [playerToIgnore,this.clownLast[this.random.integerInRange(0, this.clownLast.length)]]
+            ],true);
+        }else if(!itemReceived && hadItem && clownEmpty){
+             this.dialog.setTextArray([
+                [playerToIgnore,this.clownGiveItemJokes[this.random.integerInRange(0, this.clownGiveItemJokes.length-1)]],
+                 [playerToIgnore,this.clownLast[this.random.integerInRange(0, this.clownLast.length)]]
+            ],true);
+        }
+        else if(itemReceived && hadItem && !clownEmpty){
+            this.dialog.setTextArray([
+                [playerToIgnore,this.clownNoObjectTaken[this.random.integerInRange(0, this.clownNoObjectTaken.length-1)]],
+                 [playerToIgnore,this.clownLast[this.random.integerInRange(0, this.clownLast.length)]]
+            ],true);
+        }
+        else if(!itemReceived && !hadItem && clownEmpty){
+            this.dialog.setTextArray([
+                [playerToIgnore,this.clownNoObjectGiven[this.random.integerInRange(0, this.clownNoObjectGiven.length-1)]],
+                 [playerToIgnore,this.clownLast[this.random.integerInRange(0, this.clownLast.length)]]
+            ],true);
+        }
     }
     
     addMapLayer(name,collision){
